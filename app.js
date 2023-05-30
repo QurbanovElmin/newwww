baeded4160892ba801c99195e07195ed3a5481bf
QurbanovElmin <7p0063u@code.edu.az>
2023-05-30 23:06:51 +0400

    supi

---
+++ app.js
@@ -0,0 +1,42 @@
+const arr = [ 7,55,32,91,87,21,74,252,2,-6,42,15,-41,-8]
+// cut ededleri cixardir 
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]%2==0){
+//         console.log( arr[i]);       
+//     }
+// }
+
+//tek edeleri cixardir
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]%2==-1 || arr[i]%2==1 ){
+//         console.log( arr[i]);       
+//     }
+// }
+// menfi ededleri ekrana cixarmaq 
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]%2==-1 || arr[i]%4==-2 )
+//         console.log( arr[i]);       
+//     }
+
+
+//  her bir ededin kvadrati
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]**=2){
+//         console.log( arr[i]);      
+//     }
+// }
+
+// her bir ededin 2 qati
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]*=2){
+//         console.log( arr[i]);      
+//     }
+// }
+
+//40 dan boyuk olan ededler
+
+// for ( let i=0; i<arr.length;i++){
+//     if( arr[i]>40){
+//         console.log( arr[i]);      
+//     }
+// }
